79d03ce5a90e531bc6b5abe3379e64c1e54cba8f
Kevin W Li <kevinwli@outlook.com>
2016-08-21 23:41:44 -0400

    corrected base parameterisation

--- shapes.py
+++ shapes.py
@@ -1,6 +1,6 @@
 import pymunk
 
-def add_block(space, position = [0.0,0.0], mass = 1, size = 10):
+def add_block(space, position = [0.0,0.0], mass = 10, size = 10):
 
     coord = size/2
     coords = [[-coord, -coord], [coord,-coord], [coord, coord], [-coord,coord]]
@@ -9,14 +9,18 @@ def add_block(space, position = [0.0,0.0], mass = 1, size = 10):
     body.position = position
     shape = pymunk.Poly(body, coords)
     space.add(body,shape)
+    shape.friction = 1000000.0
+    shape.elasticity = 0.0
     return shape
 
 
 def add_base(space, p1, p2):
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
-    mid =  (p1[0]+p2[0])/2, (p1[1]+p2[1])/2
+    mid =  p1[0], p1[1]
     body.position = mid
-    l = pymunk.Segment(body, p1, p2, 10)
-    space.add(l)
-    return l
+    shape = pymunk.Segment(body, (0,0), (p2[0]-p1[0],p2[1]-p1[1]), 10)
+    space.add(shape)
+    shape.friction = 1000000.0
+    shape.elasticity = 0.0
+    return shape
 
