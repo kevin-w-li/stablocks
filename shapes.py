c044afda278d6963116090962d9c68bb9f5809fc
naji <naji.shajari@gmail.com>
2016-08-21 23:44:19 -0400

    fuck you!

--- shapes.py
+++ shapes.py
@@ -1,6 +1,6 @@
 import pymunk
 
-def add_block(space, position = [0.0,0.0], mass = 1, size = 10):
+def add_block(space, position = [0.0, 0.0], mass = 1, size = 10):
 
     coord = size/2
     coords = [[-coord, -coord], [coord,-coord], [coord, coord], [-coord,coord]]
@@ -9,7 +9,18 @@ def add_block(space, position = [0.0,0.0], mass = 1, size = 10):
     body.position = position
     shape = pymunk.Poly(body, coords)
     space.add(body,shape)
-    return shape
+    return body, shape
+
+def make_pile(space, num_of_blocks = 5, base_coord = [(0, 100), (500, 100)], mass = 1, size = 10):
+    add_base(space, base_coord[0], base_coord[1])
+    first_block_pos = [(base_coord[0][0] + base_coord[1][0]) / 2., base_coord[0][1]]
+    shape_list = []
+    body_list = []
+    for i in range(num_of_blocks):
+        body, shape = add_block(space, first_block_pos , mass = mass, size = size)
+        body_list.append(body)
+        shape_list.append(shape)
+    return body_list, shape_list
 
 
 def add_base(space, p1, p2):
