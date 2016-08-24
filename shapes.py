20ea6b6bd4774ff1301b1474e45228f4bc13d76d
Kevin W Li <kevinwli@outlook.com>
2016-08-24 18:20:46 -0400

    convert pymunk space into numpy array

--- shapes.py
+++ shapes.py
@@ -1,26 +1,42 @@
 import pymunk
 
-def add_block(space, position = [0.0,0.0], mass = 10, size = 10):
+def add_block(space, position = [0.0,0.0], mass = 1, size = 10):
 
     coord = size/2
     coords = [[-coord, -coord], [coord,-coord], [coord, coord], [-coord,coord]]
     moment = pymunk.moment_for_poly(mass, coords)
     body = pymunk.Body(mass,moment)
     body.position = position
-    shape = pymunk.Poly(body, coords)
+    shape = pymunk.Poly(body,coords)
     space.add(body,shape)
-    shape.friction = 1000000.0
+    shape.friction = 1000.0
     shape.elasticity = 0.0
     return shape
 
 
-def add_base(space, p1, p2):
+def make_pile(space, num_of_blocks = 5, base_coord = [(0, 100), (500, 100)], mass = 1, size = 10):
+    add_base(space, base_coord[0], base_coord[1])
+    first_block_pos = [(base_coord[0][0] + base_coord[1][0]) / 2., base_coord[0][1]]
+    shape_list = []
+    body_list = []
+    for i in range(num_of_blocks):
+        body, shape = add_block(space, first_block_pos , mass = mass, size = size)
+        body_list.append(body)
+        shape_list.append(shape)
+    return body_list, shape_list
+
+def add_base(space, p1, p2, thick = 5):
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
-    mid =  p1[0], p1[1]
+    mid =  (p1[0] + p2[0])/2.0, (p1[1] + p2[1])/2.0
+    half_length = (p2[0]-p1[0])/2.0
     body.position = mid
-    shape = pymunk.Segment(body, (0,0), (p2[0]-p1[0],p2[1]-p1[1]), 10)
+    coords = [[-half_length,-thick], [+half_length,-thick], \
+        [-half_length, +thick], [+half_length, +thick]]
+    shape = pymunk.Poly(body, coords)
     space.add(shape)
-    shape.friction = 1000000.0
+    shape.friction = 1000.0
     shape.elasticity = 0.0
     return shape
+
+
 
