Truncnorm corrected with a constant variance scale...

try_pymunk.py
@@ -3,10 +3,10 @@ import pygame
 from pygame.locals import *
 import pymunk #1
 import random
-from shapes import add_block, add_base, make_pile_given_noise, apply_noise
+from shapes import add_block, add_base, make_pile_given_noise
 from scipy.stats import truncnorm, uniform
 import time
-
+import numpy as np
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
@@ -40,26 +40,31 @@ while True:
     #     s = add_block(space, position = [300,500], size = 50)
     #     blocks.append(s)
 
-    screen.fill((255,255,255))
     for i in range(block_arrangements_num):
-        for j in range(noise_trials):
-            space = pymunk.Space()
-            space.gravity = (0.0, -900.0)
-            space.step(1 / 50.0)
-            draw_options = pygame_util.DrawOptions(screen)
-            noise_list = truncnorm.rvs(- 1. / (var), 1. / (var), size = num_of_blocks) * var
-            s_list, b_list = make_pile_given_noise(space=space, noise_list=noise_list)
-            apply_noise(s_list, num_of_trials=3)
-            space.debug_draw(draw_options)
-            for k in range(len(s_list)):
-                space.remove(b_list[0])
-                space.remove(s_list[0])
-                b_list.remove(b_list[0])
-                s_list.remove(s_list[0])
-            del s_list
-            del b_list
-            pygame.display.flip()
-    clock.tick(50)
+        position_noise_list = truncnorm.rvs(- 1. / (var), 1. / (var), size=num_of_blocks) * var
+        hor_ver_list = np.random.binomial(2, 0.5, num_of_blocks)
+        for j in range(num_of_blocks):
+            for k in range(noise_trials):
+                noise = np.random.randn(var)
+                space = pymunk.Space()
+                space.gravity = (0.0, -900.0)
+                draw_options = pygame_util.DrawOptions(screen)
+                print i, j
+                b_list, s_list = make_pile_given_noise(space=space, noise_list=position_noise_list,
+                                                       hor_ver_list=hor_ver_list)
+                for step_num in range(300):
+                    screen.fill((255, 255, 255))
+                    space.debug_draw(draw_options)
+                    if step_num == 100:
+                        b_list[j].apply_force_at_local_point([noise, 0.], b_list[j].position)
+                    space.step(1 / 50.0)
+                    clock.tick(50)
+                    pygame.display.flip()
+                for k in range(len(s_list)):
+                    space.remove(b_list[0])
+                    space.remove(s_list[0])
+                    b_list.remove(b_list[0])
+                    s_list.remove(s_list[0])
 
 
 